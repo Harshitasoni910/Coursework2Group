aa4deedb92c7d1c9048db92412d7dc58ab077337
Harshitasoni910 <85622653+Harshitasoni910@users.noreply.github.com>
2023-01-10 23:43:38 +0000

    Update lessons.js

--- routes/lessons.js
+++ routes/lessons.js
@@ -1,1 +1,42 @@
+const  app = require('express');
+const express = require('express');
+ 
+const router = express.Router();
 
+ 
+let products = [
+    {
+     'Topic': 'math', 
+     'Location' : 'Hendon' , 
+     'Price': "100"
+    },
+
+
+    {
+    'Topic': 'math',
+     'Location' : 'Colindale' , 
+     'Price': "80"
+    },
+
+
+    {
+     'Topic': 'math',
+     'Location' : 'Brent Cross' , 
+     'Price': "90"
+    },
+
+
+    {
+      'Topic': 'math', 
+      'Location' : 'Golder Green' , 
+      'Price': "120"
+    }
+]
+ 
+
+router.get('/lessons', (req,res)=> 
+    res.json(products)
+);
+
+
+module.exports = router;
