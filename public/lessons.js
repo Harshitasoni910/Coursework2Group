43df61fcd3da800b0bf97d2a63273b0d938382ef
Harshitasoni910 <85622653+Harshitasoni910@users.noreply.github.com>
2023-01-10 23:45:58 +0000

    Add files via upload

---
+++ public/lessons.js
@@ -0,0 +1,32 @@
+let products = [
+  {
+    'Topic': 'math', 
+    'Location' : 'Hendon' , 
+    'Price': "100"
+   },
+
+
+   {
+   'Topic': 'math',
+    'Location' : 'Colindale' , 
+    'Price': "80"
+   },
+
+
+   {
+    'Topic': 'math',
+    'Location' : 'Brent Cross' , 
+    'Price': "90"
+   },
+
+
+   {
+     'Topic': 'math', 
+     'Location' : 'Golder Green' , 
+     'Price': "120"
+   }
+    
+     
+
+    
+]
